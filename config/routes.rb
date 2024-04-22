c10799fb192891a3f48e528e8e255c588258cd38
Nikolaos Emmanouilidis <121699424+NikEmman@users.noreply.github.com>
2024-04-23 01:08:59 +0300

    Add navbar partial and its links

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   
   devise_for :users, :controllers => { registrations: 'users/registrations' }
   devise_scope :user do
+   get '/users/sign_out' => 'devise/sessions#destroy'
    get 'users/:id', to: 'users/registrations#show', as: 'user'
   end
   get "events/new", to: 'events#new'
